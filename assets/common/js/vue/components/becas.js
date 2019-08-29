a855f6a5cca44ce3a974e62f123eb381ce124288
jfescobar18 <jfescobar18@hotmail.com>
2019-08-29 10:07:13 -0500

    API integrated

--- assets/common/js/vue/components/becas.js
+++ assets/common/js/vue/components/becas.js
@@ -1,4 +1,5 @@
 var Becas = Vue.component('Becas', {
+    props: ['PDFs'],
     methods: {
         AddActiveLink: function () {
             var links = document.getElementsByClassName("navbar-link");
@@ -6,6 +7,25 @@ var Becas = Vue.component('Becas', {
                 links[i].className = links[i].className.replace(" active-link", "");
             }
             links[2].className += " active-link";
+            this.GetPdfs();
+        },
+        GetPdfs: function () {
+            this.$http.get('http://api.fefarm.mx/GrantPDF/GetGrantPDFs').then(
+                response => {
+                    console.log(response.body);
+                    if (response.status === 200) {
+                        this.PDFs = response.body;
+                    }
+                },
+                err => {
+                    console.log(err);
+                    swal({
+                        icon: "error",
+                        title: "¡Ups!",
+                        text: "Algo ha ido mal, intenta más tarde"
+                    });
+                }
+            );
         }
     },
     template: `
@@ -13,36 +33,15 @@ var Becas = Vue.component('Becas', {
             <section-banner></section-banner>
             <div class="pdf-container">
 
-                <div class='pdf-card'>
+                <div class='pdf-card' v-for="pdf in PDFs">
                     <div class='wave -one'></div>
                     <div class='wave -two'></div>
                     <div class='wave -three'></div>
-                    <div class='title'>TitleTitleTitleTitleTitle</div>
-                </div>
-                <div class='pdf-card'>
-                    <div class='wave -one'></div>
-                    <div class='wave -two'></div>
-                    <div class='wave -three'></div>
-                    <div class='title'>TitleTitleTitleTitleTitle</div>
-                </div>
-                <div class='pdf-card'>
-                    <div class='wave -one'></div>
-                    <div class='wave -two'></div>
-                    <div class='wave -three'></div>
-                    <div class='title'>TitleTitleTitleTitleTitle</div>
-                </div>
-
-                <div class='pdf-card'>
-                    <div class='wave -one'></div>
-                    <div class='wave -two'></div>
-                    <div class='wave -three'></div>
-                    <div class='title'>TitleTitleTitleTitleTitle</div>
-                </div>
-                <div class='pdf-card'>
-                    <div class='wave -one'></div>
-                    <div class='wave -two'></div>
-                    <div class='wave -three'></div>
-                    <div class='title'>TitleTitleTitleTitleTitle</div>
+                    <div class='title'>
+                        <a v-bind:href="'http://api.fefarm.mx/' + pdf.Grant_PDF_Path" target="_target">
+                            {{pdf.Grant_PDF_Name}}
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
