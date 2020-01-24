78c5b71437a04cbb424e6eb6f6558c6cfa72ab8d
jfescobar18 <jfescobar18@hotmail.com>
2020-01-24 11:05:06 -0600

    Ref updated

--- assets/common/js/main.js
+++ assets/common/js/main.js
@@ -1,18 +1,18 @@
-import navibar from './vue/components/navibar.js?n=1.1';
-import mainBanner from './vue/components/main-banner.js?n=1.1';
-import sectionBanner from './vue/components/section-banner.js?n=1.1';
-import footerContact from './vue/components/footer-contact.js?n=1.1';
-import footerMedia from './vue/components/footer-media.js?n=1.1';
-import aboutusCards from './vue/components/aboutus-cards.js?n=1.1'
-import Mision from './vue/components/mision.js?n=1.1'
-import Vision from './vue/components/vision.js?n=1.1'
-import Organigrama from './vue/components/organigrama.js?n=1.1'
-import Reglamento from './vue/components/Reglamento.js?n=1.1'
-import Solicitudes from './vue/components/Solicitudes.js?n=1.1'
-import Solicitud from './vue/components/Solicitud.js?n=1.1'
-import Evidencias from './vue/components/Evidencias.js?n=1.1'
-import Requisitos from './vue/components/Requisitos.js?n=1.1'
-import Noticias from './vue/components/Noticias.js?n=1.1'
+import navibar from './vue/components/navibar.js?n=1.2';
+import mainBanner from './vue/components/main-banner.js?n=1.2';
+import sectionBanner from './vue/components/section-banner.js?n=1.2';
+import footerContact from './vue/components/footer-contact.js?n=1.2';
+import footerMedia from './vue/components/footer-media.js?n=1.2';
+import aboutusCards from './vue/components/aboutus-cards.js?n=1.2'
+import Mision from './vue/components/mision.js?n=1.2'
+import Vision from './vue/components/vision.js?n=1.2'
+import Organigrama from './vue/components/organigrama.js?n=1.2'
+import Reglamento from './vue/components/Reglamento.js?n=1.2'
+import Solicitudes from './vue/components/Solicitudes.js?n=1.2'
+import Solicitud from './vue/components/Solicitud.js?n=1.2'
+import Evidencias from './vue/components/Evidencias.js?n=1.2'
+import Requisitos from './vue/components/Requisitos.js?n=1.2'
+import Noticias from './vue/components/Noticias.js?n=1.2'
 
 const router = new VueRouter({
     routes: [
