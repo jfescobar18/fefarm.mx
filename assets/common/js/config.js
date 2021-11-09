445ad91be2d968c0e46e5fb4ec68b434b1048ce4
jfescobar18 <jfescobar18@hotmail.com>
2021-11-09 01:15:43 -0600

    added

--- assets/common/js/config.js
+++ assets/common/js/config.js
@@ -1,5 +1,6 @@
 window.config = {
     LocalhostURL: 'http://localhost:49802/',
     ProductionURL: 'https://api.fefarm.mx/',
-    APIKey: 'yaIMUjbmh749cIv20i056xhaNbUSj8Oh'
+    APIKey: 'yaIMUjbmh749cIv20i056xhaNbUSj8Oh',
+    password: '!biR55ewQC7-D5'
 }
